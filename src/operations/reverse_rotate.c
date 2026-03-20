109218a7a4d28df3450dd39bf8340ee8110c5f58
Russell Foo <iruss1337@gmail.com>
2026-03-21 02:24:13 +0800

    feat: implement reverse rotate to put last node to top

--- src/operations/reverse_rotate.c
+++ src/operations/reverse_rotate.c
@@ -3,9 +3,18 @@
 void	reverse_rotate(t_list **stack, char *op)
 {
 	t_list	*last_node;
+	t_list	*previous_node;
 
 	if (!stack || !*stack || !(*stack)->next)
 		return ;
-	last_node = ft_lstlast(stack);
-	stack = ft_ls
+	last_node = *stack;
+	previous_node = NULL;
+	while (last_node->next)
+	{
+		previous_node = last_node;
+		last_node = last_node->next;
+	}
+	previous_node->next = NULL;
+	ft_lstadd_front(stack, last_node);
+	ft_printf("%s\n", op);
 }
