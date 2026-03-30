53a58a9462e54f13558818c6fb1dbf9a21afc5ed
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-03-30 17:11:44 +0800

    chore: clean up utils

--- src/utils/build_stack.c
+++ src/utils/build_stack.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   build_stack.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: russ1337 <russ1337@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 19:34:02 by rfoo              #+#    #+#             */
-/*   Updated: 2026/03/19 20:47:55 by russ1337         ###   ########.fr       */
+/*   Updated: 2026/03/30 17:10:58 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int	is_integer(char *str);
+static int	is_duplicate(size_t last_index, long *num_arr, long num);
+static int	add_to_stack(t_list **stack, long *num_arr, long num);
+static void	*handle_invalid_stack(long *num_arr, t_list **stack);
 
 t_list	*build_stack(size_t size, char **nums)
 {
@@ -33,9 +38,64 @@ t_list	*build_stack(size_t size, char **nums)
 			return (handle_invalid_stack(num_arr, &stack));
 		num_arr[i] = num;
 		if (!add_to_stack(&stack, num_arr, num))
-		return (handle_invalid_stack(num_arr, &stack));
+			return (handle_invalid_stack(num_arr, &stack));
 		i++;
 	}
 	free(num_arr);
 	return (stack);
+}
+
+static int	is_integer(char *str)
+{
+	int		i;
+	long	num;
+
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	num = ft_atol(str);
+	if (num > INT_MAX || num < INT_MIN)
+		return (0);
+	return (1);
+}
+
+static int	is_duplicate(size_t last_index, long *num_arr, long num)
+{
+	int	i;
+
+	i = 0;
+	while (i < last_index)
+	{
+		if (num == num_arr[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	add_to_stack(t_list **stack, long *num_arr, long num)
+{
+	long	*content;
+
+	content = malloc(sizeof(long));
+	if (!content)
+		return (0);
+	*content = num;
+	ft_lstadd_back(stack, ft_lstnew(content));
+	return (1);
+}
+
+static void	*handle_invalid_stack(long *num_arr, t_list **stack)
+{
+	free(num_arr);
+	ft_lstclear(stack, free);
+	return (NULL);
 }
--- src/utils/handle_invalid_stack.c
+++
@@ -1,9 +0,0 @@
-#include "push_swap.h"
-
-void	*handle_invalid_stack(long *num_arr, t_list **stack)
-{
-	free(num_arr);
-	ft_lstclear(stack, free);
-	// ft_printf("Error\n");
-	return (NULL);
-}
