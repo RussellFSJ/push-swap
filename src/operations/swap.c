3e29aa4a566da15b9eb4ad8c858f7f2f57959053
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-03-30 17:19:13 +0800

    chore: add headers to operations

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/30 17:18:08 by rfoo              #+#    #+#             */
+/*   Updated: 2026/03/30 17:18:51 by rfoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 void	swap(t_list **stack, char *op)
@@ -12,5 +24,5 @@ void	swap(t_list **stack, char *op)
 	first_node->next = second_node->next;
 	second_node->next = first_node;
 	*stack = second_node;
-	ft_printf("%s\n", op);	
+	ft_printf("%s\n", op);
 }
