8df19ab75b944bc1fa5b71cb7366b3253fbd62b8
Russell Foo <iruss1337@gmail.com>
2026-05-15 06:00:46 +0800

    feat: add sort_three_to_five to handle stack size between 3 and 5

--- src/sort/small_sort.c
+++ src/sort/small_sort.c
@@ -6,27 +6,27 @@
 /*   By: russ1337 <russ1337@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 17:19:41 by rfoo              #+#    #+#             */
-/*   Updated: 2026/05/15 03:09:49 by russ1337         ###   ########.fr       */
+/*   Updated: 2026/05/15 05:59:25 by russ1337         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void sort_three(t_list **stack);
+static void	sort_three(t_list **stack);
+static void	sort_three_to_five(t_list **a, t_list **b, size_t stack_size);
 
-void small_sort(t_list **a, t_list **b, size_t stack_size)
+void	small_sort(t_list **a, t_list **b, size_t stack_size)
 {
-	if (stack_size < 2 || is_sorted(a))
-		return ;
-	else if (stack_size == 2)
-		sa(a);
-	else if (stack_size >= 3)
+	if (stack_size >= 2 && !is_sorted(a))
 	{
-		sort_three(a);
+		if (stack_size == 2)
+			sa(a);
+		else if (stack_size >= 3)
+			sort_three_to_five(a, b, stack_size);
 	}
 }
 
-static void sort_three(t_list **stack)
+static void	sort_three(t_list **stack)
 {
 	int	first;
 	int	second;
@@ -51,5 +51,21 @@ static void sort_three(t_list **stack)
 	{
 		sa(stack);
 		rra(stack);
+	}
+}
+
+static void	sort_three_to_five(t_list **a, t_list **b, size_t stack_size)
+{
+	int	min;
+
+	if (stack_size == 3)
+		sort_three(a);
+	else
+	{
+		min = find_min(a);
+		move_to_top(a, min);
+		pb(a, b);
+		small_sort(a, b , stack_size - 1);
+		pa(a, b);
 	}
 }
