477b17f3b5a969a5919358c7ba8070966c70e36d
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-05-14 16:00:22 +0800

    feat: implmented move to top to move min int to top of stack

---
+++ src/utils/move_to_top.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_to_top.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 20:16:11 by rfoo              #+#    #+#             */
+/*   Updated: 2026/05/14 15:57:43 by rfoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	move_to_top(t_list **stack, int value)
+{
+	size_t	size;
+	size_t	position;
+
+	size = stack_size(*stack);
+	position = find_position(*stack, value);
+	if (position == ((size_t) - 1))
+		return ;
+	if (position <= size / 2)
+	{
+		while ((*stack)->content != value)
+			ra(stack);
+	}
+	else
+	{
+		while ((*stack)->content != value)
+			rra(stack);
+	}
+}
