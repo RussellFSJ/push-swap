d6bf6dc03f90b216b902276b4cdd7eef5038000e
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-05-14 19:51:23 +0800

    feat: implement sort 3 to run operations manually

--- src/sort/small_sort.c
+++ src/sort/small_sort.c
@@ -6,7 +6,7 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 17:19:41 by rfoo              #+#    #+#             */
-/*   Updated: 2026/05/14 15:58:06 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/05/14 19:50:45 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,41 @@ static void	sort_three(t_list **stack);
 
 void	small_sort(t_list **a, t_list **b, size_t stack_size)
 {
-	if (stack_size < 2)
+	if (stack_size < 2 || is_sorted(a))
 		return ;
 	else if (stack_size == 2)
-	{
-		if (is_sorted(a))
-			return ;
 		sa(a);
-		return ;
-	}
 	else if (stack_size >= 3)
 	{
-		return ;
+		sort_three(a);
 	}
 }
 
 static void	sort_three(t_list **stack)
 {
-	int	i;
-	int	j;
+	int	first;
+	int	second;
+	int	third;
 
-	i = 0;
-	j = 0;
+	first = (*stack)->content;
+	second = (*stack)->next->content;
+	third = (*stack)->next->next->content;
+	
+	if (first < second && first < third && second > third)
+	{
+		sa(stack);
+		ra(stack);
+	}
+	else if (first > second && first < third)
+		sa(stack);
+	else if (first < second && first > third)
+		rra(stack);
+	else if (first > second && first > third && second < third)
+		ra(stack);
+	else if (first > second && second > first)
+	{
+		sa(stack);
+		rra(stack);
+	}
 }
 
